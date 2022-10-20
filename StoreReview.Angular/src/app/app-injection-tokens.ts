771a5e8ef281d00b0810401aadd940c1571bf35c
Aurel Starciuc <starciuc.aurel@outlook.com>
2022-10-20 17:50:56 +0300

    init

--- StoreReview.Angular/src/app/app-injection-tokens.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const STORE_REVIEW_API_URL = new InjectionToken<string>('store review api url');
--- StoreReview.Angular/src/app/shared/pipe/linkify.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { LinkifyPipe } from './linkify.pipe';
-
-@NgModule({
-    declarations: [LinkifyPipe],
-    exports: [LinkifyPipe]
-})
-export class LinkifyModule { }
